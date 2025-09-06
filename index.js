1e41aed835dd8b0f0e5b4b7349104827fb6bf012
Manoj Neupane <maanojneupane111@gmail.com>
2025-09-06 22:47:15 +0545

    update index.js file

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.get("/", (req, res) => res.render("index"));
-app.get("/admin", (req, res) => res.send("This is Admin page"));
-
-
-
-
-
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/admin", (req, res) => {
+  res.send("This is the Admin page");
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
