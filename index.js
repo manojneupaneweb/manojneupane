630610c908957bce736e00220c6351afdb2af769
Manoj Neupane <maanojneupane111@gmail.com>
2025-12-02 15:19:13 +0545

    Remove connectDB call from index.js

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ import { fileURLToPath } from "url";
 
 dotenv.config();
 const app = express();
-connectDB();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
